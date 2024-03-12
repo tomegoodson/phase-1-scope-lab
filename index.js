e4e47ca18b177d092b993f195299b1338bf696b8
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-12 10:06:20 -0500

    scopelab1.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  window.bestCustomer = 'not bob'; 
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'some value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another value';
+  }
+  
+
+  
